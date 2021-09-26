600d9f19e66215e38b572e7f92185c9e0dcd0b4a
Danila Nikolayev <D.Nikolayev@Treolan.ru>
2021-09-26 17:19:11 +0300

    script change

--- img_resizer_renamer.py
+++ img_resizer_renamer.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 
 
-def rename(path):
+def rename(path, rootdirpath):
     dirname = os.path.basename(path)
     print(f'renaming in dir: {path}, dirname: {dirname}')
 
@@ -10,30 +10,43 @@ def rename(path):
         if dirs:
             for d in dirs:
                 print(f'dir:{d}')
-                os.rename(d, d.replace(" ", "_"))
-                rename(os.path.join(dirpath, d))
+                os.rename(d, d.replace("+", " и "))
+                rename(os.path.join(dirpath, d), rootdirpath)
+        
         if files:
+            # support nesting only 1 level deep
+            # also do not operate on root dir
+            if dirpath == rootdirpath:
+                break
             n = 1
             for f in files:
                 print(f'file:{f}')
+                filepath = os.path.join(dirpath, f)
                 ext = os.path.splitext(f)[1]
-                if ext == ".py":
+                if ext != ".jpg":
                     continue
-                image = Image.open(os.path.join(dirpath, f))
+                image = Image.open(filepath)
                 image.thumbnail((800, 2000))
                 print(f'saving image: {dirname}_{n}{ext}')
-                image.save(f'{dirpath}/{dirname.replace(" ", "_")}_{n}{ext}')
+                if any((word in f for word in ("обложка", "Обложка", "thumbnail", "Thumbnail"))):
+                    image.save(f'{dirpath}/{dirname.replace(" ", "_").replace("+", "_и_")}_Обложка{ext}')
+                else:
+                    image.save(f'{dirpath}/{dirname.replace(" ", "_").replace("+", "_и_")}_{n}{ext}')
+                os.remove(filepath) #remove original file
                 n += 1
 
 
 def create_data_dict(path):
-    with open("./data.txt", 'w') as dd:
+    with open("./data.txt", 'w', encoding='utf-8') as dd:
         dd.write("[")
         for dirpath, dirs, files in os.walk(path):
             for d in dirs:
-                dd.write(f'{{title: "{d}", thumbnailImagePath: "/images/stories/{d}/thumbnail.jpg", description: "", images: [   ')
+                print(f'{{title: "{d}", id: 0, thumbnailImagePath: "/images/stories/{d}/{d.replace(" ", "_")}_Обложка.jpg", description: "", images: [   ')
+                dd.write(f'{{title: "{d}", id: 0, thumbnailImagePath: "/images/stories/{d}/{d.replace(" ", "_")}_Обложка.jpg", description: "", images: [   ')
                 for dirpath, dirs, files in os.walk(os.path.join(path, d)):
                     for f in files:
+                        if "Обложка" in f:
+                            continue
                         dd.write(f'{{title: "{os.path.splitext(f)[0]}", path: "/images/stories/{d}/{f}"}},')
                 dd.write("] }},")
         dd.write("]")
@@ -43,5 +56,5 @@ if __name__ == '__main__':
     curpath = os.path.dirname(os.path.realpath(__file__))
     print(curpath)
     print(os.walk(curpath))
-    rename(curpath)
+    rename(curpath, curpath)
     create_data_dict(curpath)
--- src/stories_data/storiesData.ts
+++ src/stories_data/storiesData.ts
@@ -13,7 +13,473 @@ export interface imageData {
 
 export const storiesData: storyData[] = [
   {
+    title: "Дарья и Борис",
     id: 1,
+    thumbnailImagePath:
+      "/images/stories/Дарья и Борис/Дарья_и_Борис_Обложка.jpg",
+    description: "",
+    images: [
+      {
+        title: "Дарья_и_Борис_1",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_1.jpg",
+      },
+      {
+        title: "Дарья_и_Борис_10",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_10.jpg",
+      },
+      {
+        title: "Дарья_и_Борис_11",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_11.jpg",
+      },
+      {
+        title: "Дарья_и_Борис_2",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_2.jpg",
+      },
+      {
+        title: "Дарья_и_Борис_3",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_3.jpg",
+      },
+      {
+        title: "Дарья_и_Борис_4",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_4.jpg",
+      },
+      {
+        title: "Дарья_и_Борис_5",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_5.jpg",
+      },
+      {
+        title: "Дарья_и_Борис_6",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_6.jpg",
+      },
+      {
+        title: "Дарья_и_Борис_7",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_7.jpg",
+      },
+      {
+        title: "Дарья_и_Борис_8",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_8.jpg",
+      },
+      {
+        title: "Дарья_и_Борис_9",
+        path: "/images/stories/Дарья и Борис/Дарья_и_Борис_9.jpg",
+      },
+    ],
+  },
+  {
+    title: "Елена",
+    id: 2,
+    thumbnailImagePath: "/images/stories/Елена/Елена_Обложка.jpg",
+    description: "",
+    images: [
+      { title: "Елена_1", path: "/images/stories/Елена/Елена_1.jpg" },
+      { title: "Елена_2", path: "/images/stories/Елена/Елена_2.jpg" },
+      { title: "Елена_3", path: "/images/stories/Елена/Елена_3.jpg" },
+      { title: "Елена_4", path: "/images/stories/Елена/Елена_4.jpg" },
+      { title: "Елена_5", path: "/images/stories/Елена/Елена_5.jpg" },
+      { title: "Елена_6", path: "/images/stories/Елена/Елена_6.jpg" },
+      { title: "Елена_7", path: "/images/stories/Елена/Елена_7.jpg" },
+    ],
+  },
+  {
+    title: "Наталья и Юрий",
+    id: 3,
+    thumbnailImagePath:
+      "/images/stories/Наталья и Юрий/Наталья_и_Юрий_Обложка.jpg",
+    description: "",
+    images: [
+      {
+        title: "Наталья_и_Юрий_1",
+        path: "/images/stories/Наталья и Юрий/Наталья_и_Юрий_1.jpg",
+      },
+      {
+        title: "Наталья_и_Юрий_2",
+        path: "/images/stories/Наталья и Юрий/Наталья_и_Юрий_2.jpg",
+      },
+      {
+        title: "Наталья_и_Юрий_3",
+        path: "/images/stories/Наталья и Юрий/Наталья_и_Юрий_3.jpg",
+      },
+      {
+        title: "Наталья_и_Юрий_4",
+        path: "/images/stories/Наталья и Юрий/Наталья_и_Юрий_4.jpg",
+      },
+      {
+        title: "Наталья_и_Юрий_5",
+        path: "/images/stories/Наталья и Юрий/Наталья_и_Юрий_5.jpg",
+      },
+      {
+        title: "Наталья_и_Юрий_6",
+        path: "/images/stories/Наталья и Юрий/Наталья_и_Юрий_6.jpg",
+      },
+    ],
+  },
+  {
+    title: "Виталий",
+    id: 4,
+    thumbnailImagePath: "/images/stories/Виталий/Виталий_Обложка.jpg",
+    description: "",
+    images: [
+      { title: "Виталий_1", path: "/images/stories/Виталий/Виталий_1.jpg" },
+      { title: "Виталий_2", path: "/images/stories/Виталий/Виталий_2.jpg" },
+      { title: "Виталий_3", path: "/images/stories/Виталий/Виталий_3.jpg" },
+      { title: "Виталий_4", path: "/images/stories/Виталий/Виталий_4.jpg" },
+      { title: "Виталий_5", path: "/images/stories/Виталий/Виталий_5.jpg" },
+      { title: "Виталий_6", path: "/images/stories/Виталий/Виталий_6.jpg" },
+      { title: "Виталий_7", path: "/images/stories/Виталий/Виталий_7.jpg" },
+      { title: "Виталий_8", path: "/images/stories/Виталий/Виталий_8.jpg" },
+      { title: "Виталий_9", path: "/images/stories/Виталий/Виталий_9.jpg" },
+    ],
+  },
+  {
+    title: "Алиса и Кира",
+    id: 5,
+    thumbnailImagePath:
+      "/images/stories/Алиса и Кира/Алиса_и_Кира_Обложка.jpg",
+    description: "",
+    images: [
+      { title: "1", path: "/images/stories/Алиса и Кира/1.jpg" },
+      { title: "2", path: "/images/stories/Алиса и Кира/2.jpg" },
+      { title: "3", path: "/images/stories/Алиса и Кира/3.jpg" },
+      { title: "4", path: "/images/stories/Алиса и Кира/4.jpg" },
+      { title: "5", path: "/images/stories/Алиса и Кира/5.jpg" },
+      { title: "6", path: "/images/stories/Алиса и Кира/6.jpg" },
+      { title: "7", path: "/images/stories/Алиса и Кира/7.jpg" },
+      { title: "8", path: "/images/stories/Алиса и Кира/8.jpg" },
+      { title: "9", path: "/images/stories/Алиса и Кира/9.jpg" },
+      { title: "10", path: "/images/stories/Алиса и Кира/10.jpg" },
+    ],
+  },
+  {
+    title: "Мария и Максим",
+    id: 6,
+    thumbnailImagePath:
+      "/images/stories/Мария и Максим/Мария_и_Максим_Обложка.jpg",
+    description: "",
+    images: [
+      {
+        title: "Мария_и_Максим_1",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_1.jpg",
+      },
+      {
+        title: "Мария_и_Максим_10",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_10.jpg",
+      },
+      {
+        title: "Мария_и_Максим_11",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_11.jpg",
+      },
+      {
+        title: "Мария_и_Максим_12",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_12.jpg",
+      },
+      {
+        title: "Мария_и_Максим_13",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_13.jpg",
+      },
+      {
+        title: "Мария_и_Максим_14",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_14.jpg",
+      },
+      {
+        title: "Мария_и_Максим_15",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_15.jpg",
+      },
+      {
+        title: "Мария_и_Максим_2",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_2.jpg",
+      },
+      {
+        title: "Мария_и_Максим_3",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_3.jpg",
+      },
+      {
+        title: "Мария_и_Максим_4",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_4.jpg",
+      },
+      {
+        title: "Мария_и_Максим_5",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_5.jpg",
+      },
+      {
+        title: "Мария_и_Максим_6",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_6.jpg",
+      },
+      {
+        title: "Мария_и_Максим_7",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_7.jpg",
+      },
+      {
+        title: "Мария_и_Максим_8",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_8.jpg",
+      },
+      {
+        title: "Мария_и_Максим_9",
+        path: "/images/stories/Мария и Максим/Мария_и_Максим_9.jpg",
+      },
+    ],
+  },
+
+  {
+    title: "Оксана",
+    id: 7,
+    thumbnailImagePath: "/images/stories/Оксана/Оксана_Обложка.jpg",
+    description: "",
+    images: [
+      { title: "Оксана_1", path: "/images/stories/Оксана/Оксана_1.jpg" },
+      { title: "Оксана_2", path: "/images/stories/Оксана/Оксана_2.jpg" },
+      { title: "Оксана_3", path: "/images/stories/Оксана/Оксана_3.jpg" },
+      { title: "Оксана_4", path: "/images/stories/Оксана/Оксана_4.jpg" },
+      { title: "Оксана_5", path: "/images/stories/Оксана/Оксана_5.jpg" },
+      { title: "Оксана_6", path: "/images/stories/Оксана/Оксана_6.jpg" },
+      { title: "Оксана_7", path: "/images/stories/Оксана/Оксана_7.jpg" },
+      { title: "Оксана_8", path: "/images/stories/Оксана/Оксана_8.jpg" },
+    ],
+  },
+  {
+    title: "Кристина и Яна",
+    id: 8,
+    thumbnailImagePath:
+      "/images/stories/Кристина и Яна/Кристина_и_Яна_Обложка.jpg",
+    description: "",
+    images: [
+      {
+        title: "Кристина_и_Яна_1",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_1.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_10",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_10.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_11",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_11.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_12",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_12.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_13",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_13.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_2",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_2.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_3",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_3.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_4",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_4.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_5",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_5.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_6",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_6.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_7",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_7.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_8",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_8.jpg",
+      },
+      {
+        title: "Кристина_и_Яна_9",
+        path: "/images/stories/Кристина и Яна/Кристина_и_Яна_9.jpg",
+      },
+    ],
+  },
+  {
+    title: "Дарья",
+    id: 9,
+    thumbnailImagePath: "/images/stories/Дарья/Дарья_Обложка.jpg",
+    description: "",
+    images: [
+      { title: "Дарья_1", path: "/images/stories/Дарья/Дарья_1.jpg" },
+      { title: "Дарья_2", path: "/images/stories/Дарья/Дарья_2.jpg" },
+      { title: "Дарья_3", path: "/images/stories/Дарья/Дарья_3.jpg" },
+      { title: "Дарья_4", path: "/images/stories/Дарья/Дарья_4.jpg" },
+      { title: "Дарья_5", path: "/images/stories/Дарья/Дарья_5.jpg" },
+      { title: "Дарья_6", path: "/images/stories/Дарья/Дарья_6.jpg" },
+      { title: "Дарья_7", path: "/images/stories/Дарья/Дарья_7.jpg" },
+      { title: "Дарья_8", path: "/images/stories/Дарья/Дарья_8.jpg" },
+      { title: "Дарья_9", path: "/images/stories/Дарья/Дарья_9.jpg" },
+    ],
+  },
+
+
+  {
+    title: "Мария и Андрей",
+    id: 10,
+    thumbnailImagePath:
+      "/images/stories/Мария и Андрей/Мария_и_Андрей_Обложка.jpg",
+    description: "",
+    images: [
+      { title: "1", path: "/images/stories/Мария и Андрей/1.jpg" },
+      { title: "10", path: "/images/stories/Мария и Андрей/10.jpg" },
+      { title: "2", path: "/images/stories/Мария и Андрей/2.jpg" },
+      { title: "3", path: "/images/stories/Мария и Андрей/3.jpg" },
+      { title: "4", path: "/images/stories/Мария и Андрей/4.jpg" },
+      { title: "5", path: "/images/stories/Мария и Андрей/5.jpg" },
+      { title: "6", path: "/images/stories/Мария и Андрей/6.jpg" },
+      { title: "7", path: "/images/stories/Мария и Андрей/7.jpg" },
+      { title: "8", path: "/images/stories/Мария и Андрей/8.jpg" },
+      { title: "9", path: "/images/stories/Мария и Андрей/9.jpg" },
+      {
+        title: "Мария+Анрей (36)",
+        path: "/images/stories/Мария и Андрей/Мария+Анрей (36).jpg",
+      },
+      {
+        title: "Мария+Анрей (50)",
+        path: "/images/stories/Мария и Андрей/Мария+Анрей (50).jpg",
+      },
+      {
+        title: "Мария+Анрей (9)",
+        path: "/images/stories/Мария и Андрей/Мария+Анрей (9).jpg",
+      },
+    ],
+  },
+  {
+    title: "Александр",
+    id: 11,
+    thumbnailImagePath: "/images/stories/Александр/Александр_Обложка.jpg",
+    description: "",
+    images: [
+      {
+        title: "Александр_1",
+        path: "/images/stories/Александр/Александр_1.jpg",
+      },
+      {
+        title: "Александр_10",
+        path: "/images/stories/Александр/Александр_10.jpg",
+      },
+      {
+        title: "Александр_11",
+        path: "/images/stories/Александр/Александр_11.jpg",
+      },
+      {
+        title: "Александр_12",
+        path: "/images/stories/Александр/Александр_12.jpg",
+      },
+      {
+        title: "Александр_13",
+        path: "/images/stories/Александр/Александр_13.jpg",
+      },
+      {
+        title: "Александр_14",
+        path: "/images/stories/Александр/Александр_14.jpg",
+      },
+      {
+        title: "Александр_15",
+        path: "/images/stories/Александр/Александр_15.jpg",
+      },
+      {
+        title: "Александр_16",
+        path: "/images/stories/Александр/Александр_16.jpg",
+      },
+      {
+        title: "Александр_17",
+        path: "/images/stories/Александр/Александр_17.jpg",
+      },
+      {
+        title: "Александр_18",
+        path: "/images/stories/Александр/Александр_18.jpg",
+      },
+      {
+        title: "Александр_19",
+        path: "/images/stories/Александр/Александр_19.jpg",
+      },
+      {
+        title: "Александр_2",
+        path: "/images/stories/Александр/Александр_2.jpg",
+      },
+      {
+        title: "Александр_20",
+        path: "/images/stories/Александр/Александр_20.jpg",
+      },
+      {
+        title: "Александр_21",
+        path: "/images/stories/Александр/Александр_21.jpg",
+      },
+      {
+        title: "Александр_3",
+        path: "/images/stories/Александр/Александр_3.jpg",
+      },
+      {
+        title: "Александр_4",
+        path: "/images/stories/Александр/Александр_4.jpg",
+      },
+      {
+        title: "Александр_5",
+        path: "/images/stories/Александр/Александр_5.jpg",
+      },
+      {
+        title: "Александр_6",
+        path: "/images/stories/Александр/Александр_6.jpg",
+      },
+      {
+        title: "Александр_7",
+        path: "/images/stories/Александр/Александр_7.jpg",
+      },
+      {
+        title: "Александр_8",
+        path: "/images/stories/Александр/Александр_8.jpg",
+      },
+      {
+        title: "Александр_9",
+        path: "/images/stories/Александр/Александр_9.jpg",
+      },
+    ],
+  },
+
+  {
+    title: "Анастасия",
+    id: 12,
+    thumbnailImagePath: "/images/stories/Анастасия/Анастасия_Обложка.jpg",
+    description: "",
+    images: [
+      {
+        title: "Анастасия_1",
+        path: "/images/stories/Анастасия/Анастасия_1.jpg",
+      },
+      {
+        title: "Анастасия_2",
+        path: "/images/stories/Анастасия/Анастасия_2.jpg",
+      },
+      {
+        title: "Анастасия_3",
+        path: "/images/stories/Анастасия/Анастасия_3.jpg",
+      },
+      {
+        title: "Анастасия_4",
+        path: "/images/stories/Анастасия/Анастасия_4.jpg",
+      },
+      {
+        title: "Анастасия_5",
+        path: "/images/stories/Анастасия/Анастасия_5.jpg",
+      },
+      {
+        title: "Анастасия_6",
+        path: "/images/stories/Анастасия/Анастасия_6.jpg",
+      },
+      {
+        title: "Анастасия_7",
+        path: "/images/stories/Анастасия/Анастасия_7.jpg",
+      },
+    ],
+  },
+
+
+
+  {
+    id: 13,
     title: "Дарья и Борис",
     thumbnailImagePath: "/images/stories/Дарья_и_Борис/Обложка.jpg",
     description: "",
@@ -97,7 +563,7 @@ export const storiesData: storyData[] = [
     ],
   },
   {
-    id: 2,
+    id: 14,
     title: "Полина",
     thumbnailImagePath: "/images/stories/Полина/Обложка.jpg",
     description: "",
@@ -135,7 +601,7 @@ export const storiesData: storyData[] = [
     ],
   },
   {
-    id: 3,
+    id: 15,
     title: "Дарья и Роман",
     thumbnailImagePath: "/images/stories/Дарья_и_Роман/Обложка.jpg",
     description: "",
@@ -215,7 +681,7 @@ export const storiesData: storyData[] = [
     ],
   },
   {
-    id: 4,
+    id: 16,
     title: "Екатерина",
     thumbnailImagePath: "/images/stories/Екатерина/Обложка.jpg",
     description: "",
@@ -291,7 +757,7 @@ export const storiesData: storyData[] = [
     ],
   },
   {
-    id: 5,
+    id: 17,
     title: "Александр и Александра",
     thumbnailImagePath: "/images/stories/Александр_и_Александра/Обложка.jpg",
     description: "",
@@ -351,7 +817,7 @@ export const storiesData: storyData[] = [
     ],
   },
   {
-    id: 6,
+    id: 18,
     title: "Елизавета и Чизу",
     thumbnailImagePath: "/images/stories/Елизавета_и_Чизу/Обложка.jpg",
     description: "",
@@ -435,7 +901,7 @@ export const storiesData: storyData[] = [
     ],
   },
   {
-    id: 7,
+    id: 19,
     title: "Андрей и Елизавета",
     thumbnailImagePath: "/images/stories/Андрей_и_Елизавета/Обложка.jpg",
     description: "",
@@ -508,7 +974,7 @@ export const storiesData: storyData[] = [
   },
 
   {
-    id: 8,
+    id: 20,
     title: "Станислав",
     thumbnailImagePath: "/images/stories/Станислав/Обложка.jpg",
     description: "",
@@ -569,7 +1035,7 @@ export const storiesData: storyData[] = [
   },
 
   {
-    id: 9,
+    id: 21,
     title: "Ксения и Михаил",
     thumbnailImagePath: "/images/stories/Ксения_и_Михаил/Обложка.jpg",
     description: "",
